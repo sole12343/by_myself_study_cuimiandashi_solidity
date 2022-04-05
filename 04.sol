f26fa890e0c36405a0313f7c937be3075b512173
sole <52155079+sole12343@users.noreply.github.com>
2022-04-05 10:33:21 +0800

    Create 04.sol

---
+++ 04.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.7;
+
+contract functionIntro{
+    function add(uint x , uint y ) external pure returns (uint) {
+        return x + y ;
+    }
+    function sub(uint x , uint y ) external pure returns (uint) {
+        return x - y ;
+    }
+}
