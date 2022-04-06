894cfa838df2687969535408f9a641e1be2a88fe
sole <52155079+sole12343@users.noreply.github.com>
2022-04-06 20:36:21 +0800

    Create defaultvalue.sol

---
+++ defaultvalue.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7 ;
+
+contract defaultvalue{
+    bool public b ;//false
+    uint public u ;//0
+    int public i ;//0
+    address public address_a ;//0x0000000000000000000000000000000000000000
+    bytes32 public b32 ;      //0x0000000000000000000000000000000000000000000000000000000000000000
+}
